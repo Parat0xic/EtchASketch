ead429b10b7336f0a201b66c499eac61b2fa4d96
Parat0xic <zipling36@gmail.com>
2023-03-28 14:52:03 -0400

    Add button for set squares and button for reset. Make function for random color and for buttons.

--- javascript.js
+++ javascript.js
@@ -1,37 +1,64 @@
 
-
-// Prompt user for  X number of squares wide when they press a button at the top of the screen
-//Base site should be 16x16 and only change if user clicks button
-//generate X divs horizontal filled with X divs vertical (or opposite might work better)
-//place a hover event listener on each div that changes color of div
-//adjust div spacing and margins and borders and stuff
-//changing size should not change area used just density
-
-//Generates 16 x 16 base grid of divs
-
-
-
+//set container to flex and row wrap
 const containerdiv = document.querySelector(".container");
 containerdiv.style.display = "flex";
 containerdiv.style.flexFlow = "row wrap";
-let squares = 4;
-let squared = squares*squares;
-let horizontalsize = "calc(100% / " + squares + ")";
-let horizontalsizeappend = horizontalsize + "%";
-let xdivs;
 
-for (let i = 0; i < squared; i++) {
-    xdivs = document.createElement("div");
-    xdivs.classList.add("xdiv");
-    xdivs.style.flexBasis = horizontalsize;
-    xdivs.style.height = horizontalsize;
-    xdivs.style.padding = 0;
-    xdivs.style.margin = 0;
-    xdivs.style.backgroundColor = "Red";
-    containerdiv.appendChild(xdivs);
-  
+let squares = 16;
+drawsquares(squares);
+
+function drawsquares(squares) {
+    let squared = squares * squares;
+    let horizontalsize = "calc(100% / " + squares + ")";
+    let horizontalsizeappend = horizontalsize + "%";
+    for (let i = 0; i < squared; i++) {
+        let xdivs = document.createElement("div");
+        xdivs.classList.add("xdiv");
+        xdivs.style.flexBasis = horizontalsize;
+        //xdivs.style.height = horizontalsize; Unsure if I need this or not
+        xdivs.style.padding = 0;
+        xdivs.style.margin = 0;
+        xdivs.style.backgroundColor = "White";
+        xdivs.addEventListener("mouseover", () => {
+            xdivs.style.backgroundColor = randomcolor();
+        })
+        containerdiv.appendChild(xdivs);
+        
+    }
+}
+
+//queryselector for setsquares button
+const setsquaresbtn = document.querySelector(".button");
+//eventListener onto set squares button to set squares
+setsquaresbtn.addEventListener("click", () => {
+    let userinput = Number(prompt("How many squares horizontally? Max 100!"));
+
+    if (Number.isInteger(userinput) && userinput > 0 && userinput < 101) {
+        squares = userinput;
+        reset();
+        drawsquares(squares);
+    }
+    else {
+        return;
+    }
+})
+
+//queryselector for reset button
+const resetbtn = document.querySelector(".reset")
+//eventListener on resetbtn
+resetbtn.addEventListener("click", () => {
+    reset();
+    drawsquares(squares);
+})
+
+
+function reset() {
+   containerdiv.innerHTML = ''; 
 }
 
 
-
-
+//generates a random color
+function randomcolor() {
+    let randomcolor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return randomcolor;
+}
